1fd42bd62184c1a8637b32d65e1a47d7f09605a6
xmind404 <mechfranio@gmail.com>
2026-05-28 01:02:11 +0200

    Added spoofing as GoogleBot to bypass Javascript

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,19 @@ fn dork(arg: &str, inurl: bool, intext: bool, site: bool, intitle: bool) -> Stri
 
     if inurl {parts.push_str(format!("inurl:{} ", arg).as_str());}
     if intext {parts.push_str(format!("intext: {} ", arg).as_str());}
-    if site {parts.push_str(format!("site:{} ", arg).as_str());}
-    if intitle {parts.push_str(format!("intitle:{} ", arg).as_str().trim_start_matches("http://").trim_start_matches("https://"));}
+    if site {parts.push_str(format!("site:{} ", arg).as_str().trim_start_matches("http://").trim_start_matches("https://"));}
+    if intitle {parts.push_str(format!("intitle:{} ", arg).as_str());}
 
     // ========< Url generation >========
     let search = parts.as_str();
     let safe_search = urlencoding::encode(search).to_string();
     let url = format!("https://google.com/search?q={}", safe_search);
 
+    // ========< Spoofing as GoogleBot to bypass JavaScript >========
+    let client = reqwest::blocking::Client::builder().user_agent("Googlebot/2.1 (+http://google.com)").build().unwrap();
+
     // ========< Downloading site as single line >========
-    reqwest::blocking::get(&url)
-        .and_then(|res| res.text())
-        .unwrap_or_else(|_| "Network error".to_string())
+    client.get(&url).send().and_then(|res| res.text()).unwrap_or_else(|_| "Network error".to_string())
 }
 
 
