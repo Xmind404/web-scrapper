19b34c7ca9017dee12918f2882997639f2396abd
xmind404 <mechfranio@gmail.com>
2026-05-28 02:35:31 +0200

    Fixed bugs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,88 @@
+use regex::Regex;
+use std::collections::HashSet;
 fn dork(arg: &str, inurl: bool, intext: bool, site: bool, intitle: bool) -> String {
+    let configurations = [
+        (inurl, "inurl"),
+        (intext, "intext"),
+        (site, "site"),
+        (intitle, "intitle"),
+    ];
 
-    // ========< Preparation for Url >========
-    let mut parts = String::new();
 
-    if inurl {parts.push_str(format!("inurl:{} ", arg).as_str());}
-    if intext {parts.push_str(format!("intext: {} ", arg).as_str());}
-    if site {parts.push_str(format!("site:{} ", arg).as_str().trim_start_matches("http://").trim_start_matches("https://"));}
-    if intitle {parts.push_str(format!("intitle:{} ", arg).as_str());}
+    // ========< HashSet for unique links >========
+    let mut unique_links = HashSet::new();
+    let mut ordered_links = Vec::new();
 
-    // ========< Url generation >========
-    let search = parts.as_str();
-    let safe_search = urlencoding::encode(search).to_string();
-    let url = format!("https://google.com/search?q={}", safe_search);
 
-    // ========< Spoofing as GoogleBot to bypass JavaScript >========
-    let client = reqwest::blocking::Client::builder().user_agent("Googlebot/2.1 (+http://google.com)").build().unwrap();
+    // ========< Spoofing Client >========
+    let client = reqwest::blocking::Client::builder()
+        .user_agent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
+        .danger_accept_invalid_certs(true)
+        .build()
+        .unwrap();
 
-    // ========< Downloading site as single line >========
-    client.get(&url).send().and_then(|res| res.text()).unwrap_or_else(|_| "Network error".to_string())
+
+    // ========< Regex for extracting links from html >========
+    let regex = Regex::new(r#"uddg=([^&"]+)"#).unwrap();
+
+    // ========< Help variable for detecting JavaScript Challanges >========
+    let mut js_block_detected= false;
+
+    // ========< Checking every operator separately
+    for(is_enabled, operator_name) in configurations {
+
+        // ========<Cleaning start for site operator >========
+        let clean_arg = if operator_name == "site" {
+            arg.trim_start_matches("http://").trim_start_matches("https://")
+        }else{
+            arg
+        };
+
+        // ========< Url generation >========
+        let search = format!("{}:{}", operator_name, clean_arg);
+        let safe_search = urlencoding::encode(&search).to_string();
+        let url = format!("https://duckduckgo.com/html/?q={}", safe_search);
+
+        // ========< Downloading site as single line >========
+        let html_content = client.get(&url).send().and_then(|res| res.text()).unwrap_or_else(|_| "Network error".to_string());
+
+        // ========< Detect demanding JavaScript >========
+        if html_content.contains("enablejs") || html_content.contains("JavaScript") {
+            js_block_detected = true;
+        }
+
+        // ========< Decoding link from extracted links by Regex >========
+        for cap in regex.captures_iter(html_content.as_str()) {
+            if let Some(link) = cap.get(1) {
+                let found_link = link.as_str();
+
+                if let Ok(decoded) = urlencoding::decode(found_link) {
+                    let clean_link = decoded.into_owned();
+
+                    // ========< Adding cleaned link to links Vector >========
+                    if clean_link.starts_with("http") && !clean_link.contains("duckduckgo.com")  {
+                        if unique_links.insert(clean_link.clone()) {
+                            ordered_links.push(clean_link);
+                        }
+                    }
+                }
+            }
+        }
+    }
+    // ========< Return links or Error >========
+    if ordered_links.is_empty() {
+        if js_block_detected {
+            "DuckDuckGo demand JavaScript verification".to_string()
+        } else {
+            "No search results found".to_string()
+        }
+    } else {
+        ordered_links.join("\n")
+    }
 }
 
 
 
 fn main() {
-    println!("{}", dork("python.org", true, true,true,true));
+    println!("{}", dork("python.org", false, true,false,false));
 }
